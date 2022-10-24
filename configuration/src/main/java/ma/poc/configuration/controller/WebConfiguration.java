bd82303f95687d6f5e8a557a1ed674b870f08034
A.ALAA <ammar.alaaeddine@gmail.com>
2022-10-24 18:02:20 +0100

    Intégration Keycloak

--- configuration/src/main/java/ma/poc/configuration/controller/WebConfiguration.java
+++ configuration/src/main/java/ma/poc/configuration/controller/WebConfiguration.java
@@ -1,12 +1,18 @@
 package ma.poc.configuration.controller;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class WebConfiguration {
-	
-	
-	
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
 //	@Bean
 //	public WebServerFactoryCustomizer<TomcatServletWebServerFactory> customize() {
 //		return factory -> {
